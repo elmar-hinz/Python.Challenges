ebf8fbda4210a37621c3ebe3e209771e7beb1129
Elmar Hinz <t3elmar@gmail.com>
2017-03-04 16:19:54 +0100

    Adding package setup.

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name='challenge',
+    version='1.0',
+    description='Supporting library for challenges like those on stepik.org.',
+    url='https://github.com/elmar-hinz/Learn.Bioinformatics.Python',
+    author='Elmar Hinz',
+    license='MIT',
+    packages=['challenge', 'challenges.HelloWorld'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Education',
+        'License :: MIT',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Topic :: Education :: Computer Aided Instruction (CAI)',
+        ],
+    )
