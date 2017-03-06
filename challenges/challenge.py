4e0bbe29e8a60960df20e5e2b7f60c86190eecdf
Elmar Hinz <t3elmar@gmail.com>
2017-03-06 16:51:40 +0100

    Reading edges and fields of edges

--- challenges/challenge.py
+++ challenges/challenge.py
@@ -6,8 +6,10 @@ import types
 
 class Challenge:
 
+    lineBreak = '\n'
     sample = 'sample'
-    splitter = '\s+|\s?,\s?'
+    splitPattern = '\s+|\s?,\s?'
+    edgePattern = '^(\d+)->(\d+)(:(\d+))?$'
 
     def __init__(self):
         self.lines = []
@@ -49,17 +51,52 @@ class Challenge:
         return self.lines
 
     def lineToIntegers(self, line_nr):
-        return  [int(i) for i in re.compile(self.splitter)
+        return  [int(i) for i in re.compile(self.splitPattern)
                 .split(self.line(line_nr))]
 
     def lineToFloats(self, line_nr):
-        return  [float(i) for i in re.compile(self.splitter)
+        return  [float(i) for i in re.compile(self.splitPattern)
                 .split(self.line(line_nr))]
 
+    def lineToEdge(self, nr):
+        return self._toEdge(re.compile(self.edgePattern).match(self.line(nr)))
+
+    def readEdges(self, first=0, last=None):
+        nr = first
+        while True:
+            try:
+                line = self.line(nr)
+            except IndexError:
+                break
+            match = re.compile(self.edgePattern).match(line)
+            if match:
+                yield(self._toEdge(match))
+                if nr == last:
+                    break
+                else:
+                    nr = nr + 1
+            else:
+                break
+
+    def _toEdge(self, match):
+        edge = types.SimpleNamespace()
+        edge.tail = int(match.group(1))
+        edge.head = int(match.group(2))
+        if match.group(4):
+            edge.weight = int(match.group(4))
+        return edge
+
     #--------------------------------------------------
-    # Packing
+    # Formatting
     #--------------------------------------------------
 
-    def packIntegers(self):
-        self.output = ', '.join(str(x) for x in self.result)
+    def formatListOfIntegers(self, integers, joint=', '):
+        return joint.join(str(x) for x in integers)
+
+    def formatPath(self, integers, backwards=False):
+        if backwards:
+            joint = '<-'
+        else:
+            joint = '->'
+        return self.formatListOfIntegers(integers, joint)
 
