00a62b5e3a774f586b7a1fbbc7877eee60de8fb3
Elmar Hinz <t3elmar@gmail.com>
2018-01-16 11:02:44 +0100

    Removing graph and graph unit tests

--- challenges/__init__.py
+++ challenges/__init__.py
@@ -1,6 +1,5 @@
 from challenges.challenge import Challenge
 from challenges.conf import Conf
-from challenges.graph import Graph, Node, Edge
 from challenges.scaffold import Scaffold
 from challenges.runner import Runner
 from challenges.main import main
